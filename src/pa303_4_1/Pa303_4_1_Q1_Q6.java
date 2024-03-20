6cde051c00deb3eb14584f556ef96f4d42406ba6
Pinky Pappachan <pinkypappachan111@gmail.com>
2024-03-20 13:53:44 -0700

    PA303_4_1 assignment

--- src/pa303_4_1/Pa303_4_1_Q1_Q6.java
+++ src/pa303_4_1/Pa303_4_1_Q1_Q6.java
@@ -2,7 +2,7 @@ package pa303_4_1;
 
 import java.util.Scanner;
 
-public class AssignPa303_4_1 {
+public class Pa303_4_1_Q1_Q6 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
