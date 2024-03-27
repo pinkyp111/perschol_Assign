754b122870b081765c304f69540bc875d404c9d3
Pinky Pappachan <pinkypappachan111@gmail.com>
2024-03-27 13:39:52 -0700

    PA303_10_3

--- src/pa303_10_3/LibraryInterfaceDemo.java
+++ src/pa303_10_3/LibraryInterfaceDemo.java
@@ -4,12 +4,21 @@ public class LibraryInterfaceDemo {
     public static void main(String[] args) {
 
         KidUser kd = new KidUser();
-        kd.setAge(10);
+       /* kd.setAge(10);
         kd.setBooktype("kids");
-        System.out.println(kd.getAge() + " " + kd.getBooktype());
-        kd.setAge(18);
-        kd.setBooktype("fiction");
-        System.out.println(kd.getAge() + " " + kd.getBooktype());
+        kd.setAge(10);
+        kd.setBooktype("kids");*/
+        kd.registerAccount(10);
+        kd.book("kids");
+        kd.registerAccount(18);
+        kd.book("fiction");
+        kd.registerAccount(5);
+        kd.book("kids");
+        kd.registerAccount(7);
+        kd.book("fiction");
+
+
+       /* System.out.println(kd.getAge() + " " + kd.getBooktype());
         KidUser kd1 = new AdultUser();
         kd.setAge(5);
         kd.setBooktype("kids");
@@ -17,7 +26,9 @@ public class LibraryInterfaceDemo {
         kd.setAge(23);
         kd.setBooktype("fiction");
         System.out.println(kd.getAge() + " " + kd.getBooktype());
-
+        kd.setAge(4);
+        kd.setBooktype("fiction book");
+        System.out.println(kd.getAge() + " " + kd.getBooktype());*/
 
     }
 }
