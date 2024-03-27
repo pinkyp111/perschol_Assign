fca686e5c45c4f3b32290d5664b1d5cff3097d54
Pinky Pappachan <pinkypappachan111@gmail.com>
2024-03-27 14:01:41 -0700

    PA303_10_3

--- src/pa303_10_3/LibraryInterfaceDemo.java
+++ src/pa303_10_3/LibraryInterfaceDemo.java
@@ -5,25 +5,16 @@ public class LibraryInterfaceDemo {
 
         KidUser kd = new KidUser();
         KidUser kd1 = new AdultUser();
-       /* kd.setAge(10);
-        kd.setBooktype("kids");
-        kd.setAge(10);
-        kd.setBooktype("kids");*/
-        kd.registerAccount(14);
-        kd.book("kidsk");
-        kd1.registerAccount(8);
-        kd1.book("fictionll");
 
-       /* System.out.println(kd.getAge() + " " + kd.getBooktype());
-        kd.setAge(5);
-        kd.setBooktype("kids");
-        System.out.println(kd.getAge() + " " + kd.getBooktype());
-        kd.setAge(23);
-        kd.setBooktype("fiction");
-        System.out.println(kd.getAge() + " " + kd.getBooktype());
-        kd.setAge(4);
-        kd.setBooktype("fiction book");
-        System.out.println(kd.getAge() + " " + kd.getBooktype());*/
+        kd.registerAccount(10);
+        kd.book("kids");
+        kd.registerAccount(18);
+        kd.book("fiction");
+        kd1.registerAccount(5);
+        kd1.book("kids");
+        kd1.registerAccount(23);
+        kd1.book("fiction");
+
 
     }
 }
